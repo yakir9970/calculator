12fe5e65fbc44189a52800b53d554b0bdd0aa4b2
yakir9970 <yakirafeka@gmail.com>
2022-11-22 14:55:21 +0200

    Calculator design

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,7 +1,32 @@
+import Button from "./Components/Button.jsx";
+import "./styles.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>calc</h1>
+    <div className="calc-grid">
+      <div className="output">
+        <div className="prev-op">123,234 *</div>
+        <div className="curr-op">123,234</div>
+      </div>
+
+      <button className="span-two light">AC</button>
+      <button className="light">DEL</button>
+      <button className="orange">÷</button>
+      <button className="dark">7</button>
+      <button className="dark">8</button>
+      <button className="dark">9</button>
+      <button className="orange">×</button>
+      <button className="dark">4</button>
+      <button className="dark">5</button>
+      <button className="dark">6</button>
+      <button className="orange">-</button>
+      <button className="dark">1</button>
+      <button className="dark">2</button>
+      <button className="dark">3</button>
+      <button className="orange">+</button>
+      <button className="dark">0</button>
+      <button className="dark">.</button>
+      <button className="span-two orange">=</button>
     </div>
   );
 }
